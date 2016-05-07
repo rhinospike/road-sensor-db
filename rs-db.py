0dc2c796283b758a5169edd02189f1b37f6353d1
Ryan Fitzsimon <rhinospike@gmail.com>
2016-05-07 12:58:20 +1000

    Use PORT env variable

--- rs-db.py
+++ rs-db.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -10,4 +11,4 @@ def echo(text):
     return text
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=os.environ['PORT'])
