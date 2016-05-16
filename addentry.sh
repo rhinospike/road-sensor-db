37f54523f96520242066ce999dd356a9f8da02c7
Ryan Fitzsimon <rhinospike@gmail.com>
2016-05-17 01:01:48 +1000

    Update curl demo

--- addentry.sh
+++ addentry.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 SENSORID="30:52:cb:e7:ff:11"
+SERVER="https://road-sensor-db.herokuapp.com"
+#SERVER=localhost:5000
 STARTTIME=$(date -I)
 for i in `seq 1 3`; do
-	curl -vX POST localhost:5000/insert -H "Content-type: application/json" -d '{"sensorid": "'$SENSORID'","timestamp": "'$(date -Is)'","sensors":{"co": '$RANDOM', "dust": '$RANDOM', "noise": '$RANDOM'}}'
+	curl -vX POST $SERVER/insert -H "Content-type: application/json" -d '{"sensorid": "'$SENSORID'","timestamp": "'$(date -Is)'","sensors":{"co": '$RANDOM', "dust": '$RANDOM', "noise": '$RANDOM'}}'
 	sleep 1
 done
 
-curl -v localhost:5000/retrieve?sensorid=$SENSORID\&starttime=$STARTTIME
+curl -v $SERVER/retrieve?sensorid=$SENSORID\&starttime=$STARTTIME
