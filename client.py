5485b1fc224f89c54d5f24c9af83ba922466dd6b
Ryan Fitzsimon <rhinospike@gmail.com>
2016-06-07 15:16:27 +1000

    Add additional sensor fields

--- client.py
+++ client.py
@@ -2,10 +2,10 @@ import requests
 import random
 import datetime
 
-url = 'https://road-sensor-db.herokuapp.com'
-#url = "http://localhost:5000"
+#url = 'https://road-sensor-db.herokuapp.com'
+url = "http://localhost:5000"
 
-newsensor = {"sensorid":5,"latitude":random.random(),"longitude":random.random()}
+newsensor = {"sensorid": '5',"latitude":random.random(),"longitude":random.random()}
 requests.post(url + '/sensors',json=newsensor)
 
 r = requests.get(url + '/sensors')
@@ -16,7 +16,7 @@ r = requests.get(url + '/readings?')
 print(r.json())
 print()
 
-newreading = {"sensorid" : 5, "timestamp" : str(datetime.datetime.now()), "sensors":{"dust" : random.random() }}
+newreading = {"sensorid" : '5', "messageid":1, "hops":0, "timestamp" : str(datetime.datetime.now()), "sensors":{"dust" : random.random() }}
 requests.post(url + '/readings', json=[newreading])
 
 print("Final:")
--- models.py
+++ models.py
@@ -10,6 +10,8 @@ db = SQLAlchemy()
 
 SENSOR_ID = "sensorid"
 TIMESTAMP = "timestamp"
+MESSAGEID = "messageid"
+HOPS = "hops"
 SENSORS = "sensors"
 SENSOR_TYPE = "sensor"
 SENSOR_VALUE = "value"
@@ -56,41 +58,68 @@ class SensorReading(db.Model):
 
     sensorid = db.Column(db.BigInteger, ForeignKey('sensors.sensorid'), primary_key=True)
     timestamp = db.Column(db.DateTime, primary_key=True)
+    messageid = db.Column(db.Integer)
+    hops = db.Column(db.Integer)
+
     gas = db.Column(db.Float)
     dust = db.Column(db.Float)
-    noise = db.Column(db.Float)
+    luminance = db.Column(db.Float)
+    irtempambient = db.Column(db.Float)
+    irtempobject = db.Column(db.Float)
+    humidity = db.Column(db.Float)
+    humiditytemp = db.Column(db.Float)
 
     __table_args__ = (
-        CheckConstraint('NOT(gas IS NULL AND dust IS NULL AND noise IS NULL)', name='AllNullCheck'),
+        CheckConstraint('NOT(gas IS NULL AND dust IS NULL AND luminance IS NULL)', name='AllNullCheck'),
         )
 
-    def __init__(self, sensorid, timestamp, gas=None, dust=None, noise=None):
+    def __init__(self, sensorid, timestamp, messageid, hops, gas=None, dust=None, luminance=None, irtempambient=None, irtempobject=None, humidity=None, humiditytemp=None):
+        # Required
         self.sensorid = sensorid
         self.timestamp = timestamp
+        self.messageid = messageid
+        self.hops = hops
+
+        # Optional
         self.gas = gas
         self.dust = dust
-        self.noise = noise
+        self.luminance = luminance
+        self.irtempambient = irtempambient
+        self.irtempobject = irtempobject
+        self.humidity = humidity
+        self.humiditytemp = humiditytemp
 
     def fromjson(data):
         readings = data[SENSORS]
         return SensorReading(
                 MACToInt(data[SENSOR_ID]),
                 data[TIMESTAMP],
+                data[MESSAGEID],
+                data[HOPS],
                 readings.get("gas"),
                 readings.get("dust"),
-                readings.get("noise"))
+                readings.get("luminance"),
+                readings.get("irtempambient"),
+                readings.get("irtempobject"),
+                readings.get("humidity"),
+                readings.get("humiditytemp"))
 
     def tojson(self):
         return {
             "sensorid": intToMAC(self.sensorid),
             "timestamp": str(self.timestamp),
+            "messageid": self.messageid,
+            "hops": self.hops,
             "sensors": {
                 "gas": self.gas,
                 "dust": self.dust,
-                "noise": self.noise
+                "luminance": self.luminance,
+                "irtempambient": self.irtempambient,
+                "irtempobject": self.irtempobject,
+                "humidity": self.humidity,
+                "humiditytemp": self.humiditytemp
             }
         }
 
     def __repr__(self):
         return str(self.tojson())
-
